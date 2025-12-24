9bad31db4d17f5668cc942cdc6d19f757a2b3c11
pranav b <pranav.nair@tryzens.com>
2025-12-25 02:05:09 +0530

    PWA changes

---
+++ js/sworker.js
@@ -0,0 +1,19 @@
+// const { response } = require("express")
+
+self.addEventListener("install",e=>{
+
+    e.waitUntil(
+        caches.open("static").then(cache=>{
+            return cache.addAll(["./index.html"
+        ])
+        })
+    )
+})
+
+self.addEventListener("fetch",e=>{
+    e.respondWith(
+        caches.match(e.request).then(response=>{
+            return response || fetch(e.request);
+        })
+    )
+})
