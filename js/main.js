979fb9ca7eb76d6a2ac73ecab8b865b303b29953
pranav b <pranav.nair@tryzens.com>
2025-12-25 15:44:21 +0530

    upload feature

--- js/main.js
+++ js/main.js
@@ -49,3 +49,4 @@ sr.reveal('.skills__img', {delay: 400})
 sr.reveal('.work__img', {interval: 200})
 // ===== SCROLL CONTACT =====
 sr.reveal('.contact__input', {interval: 200})
+sr.reveal('.upload-container', {interval: 200})
